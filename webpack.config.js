4c40ebea885ff62a22552362b281bba1e6fa15df
Hermes <hermeschigoziri@gmail.com>
2022-04-29 23:58:12 +0100

    split up entry files

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    main: "./src/index.js",
+    sidebar: "./src/sidebar.js"
+  },
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
     static: './dist',
   },
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist/js"),
+    // clean: true,
   },
   module: {
     rules: [
