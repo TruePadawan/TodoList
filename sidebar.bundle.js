65187601156bbd8459fc70019bce59935056dd7c
Hermes <hermeschigoziri@gmail.com>
2022-04-30 00:18:54 +0100

    webpack bundled files

---
+++ sidebar.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/sidebar.js ***!
+  \************************/
+console.log('dsds');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2lkZWJhci5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSxvQiIsInNvdXJjZXMiOlsid2VicGFjazovL1lvdXJUb2RvLy4vc3JjL3NpZGViYXIuanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ2RzZHMnKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
