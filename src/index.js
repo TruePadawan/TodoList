79f44b9d6fb8f08e7ac74b424d24a60f10c23e9a
Hermes <hermeschigoziri@gmail.com>
2022-04-30 13:36:13 +0100

    make sure there is at least one project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import "./init.css";
 import "./main.css";
 import "./sidebar";
+import  { getNumberOfProjects, addNewProjectItem } from "./logic/projects/projects";
+
+// INIT
+(function _init() {
+    let projectCount = getNumberOfProjects();
+
+    if (projectCount === 0)
+    {
+        addNewProjectItem("Default");
+    }
+})();
