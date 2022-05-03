7abc3ca62a5e218568f0a1706b19553ca86a3af4
TruePadawan <hermeschigoziri@gmail.com>
2022-05-03 18:26:59 +0100

    remove unnecessary comment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import "./main.css";
 import "./logic/sidebar/sidebar";
 import "./logic/projects/projects";
 import { projectManager } from "./managers/projectManager";
-// import  { getNumberOfProjects, addNewProjectItem } from "./logic/projects/projects";
 
 // INIT
 (function _init() {
