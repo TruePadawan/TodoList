521aac336b9a8c592abccd2e486808b3ff100e89
TruePadawan <hermeschigoziri@gmail.com>
2022-05-03 13:25:19 +0100

    update import statement

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./init.css";
 import "./main.css";
 import "./logic/sidebar/sidebar";
 import "./logic/projects/projects";
-import { projectManager } from "./logic/projects/projectManager";
+import { projectManager } from "./managers/projectManager";
 // import  { getNumberOfProjects, addNewProjectItem } from "./logic/projects/projects";
 
 // INIT
