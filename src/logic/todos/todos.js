e734cedf6fbd62a55b373978c6562e00f313072d
TruePadawan <hermeschigoziri@gmail.com>
2022-05-06 23:38:10 +0100

    Update todos.js

--- src/logic/todos/todos.js
+++ src/logic/todos/todos.js
@@ -1,6 +1,23 @@
+const forms = document.querySelectorAll('dialog > form');
 const newTodoItem = document.getElementById('newItemBtn');
+
 const createTodoItemDialog = document.querySelector('.createTodoItemDialog');
+const todoItemDetailsDialog = document.querySelector('.todoItemDetailsDialog');
+
+const todoDetailsBtns = document.querySelectorAll('.showTodoDetailsBtn');
 
 newTodoItem.addEventListener('click', () => {
     createTodoItemDialog.style.display = "flex";
+});
+
+forms.forEach(form => {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+    });
+});
+
+todoDetailsBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        todoItemDetailsDialog.style.display = "flex";
+    });
 });
