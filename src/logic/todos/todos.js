db6539bc60174e4367f84161eea3142c55ae032b
TruePadawan <hermeschigoziri@gmail.com>
2022-05-07 00:47:52 +0100

    add TodoItem class

--- src/logic/todos/todos.js
+++ src/logic/todos/todos.js
@@ -1,11 +1,52 @@
+const todoItemDetailsDialog = document.querySelector('.todoItemDetailsDialog');
+const todoItemTemplate = document.getElementById('todoItemTemplate');
+
+class TodoItem {
+    title;
+    dueDate;
+    priority;
+    desc;
+    #node;
+
+    constructor(data) {
+        console.log('creating todo item');
+        
+        this.title = data.title;
+        this.dueDate = data.dueDate;
+        this.priority = data.priority;
+        this.desc = data.desc;
+
+        this.#node = this.buildDOMNode();
+    }
+    
+    buildDOMNode = () => {
+        const todoItemDOMNode = todoItemTemplate.content.firstElementChild.cloneNode(true);
+        todoItemDOMNode.querySelector('.todoItem_title').textContent = this.title;
+        todoItemDOMNode.classList.add(this.priority);
+        
+        const todoDetailsBtns = todoItemDOMNode.querySelector('.showTodoDetailsBtn');
+        todoDetailsBtns.addEventListener('click', () => {
+            todoItemDetailsDialog.style.display = "flex";
+        });
+
+        return todoItemDOMNode;
+    }
+}
+
+
+
+
+
+
+
+
 const forms = document.querySelectorAll('dialog > form');
 const newTodoItem = document.getElementById('newItemBtn');
 
 const createTodoItemDialog = document.querySelector('.createTodoItemDialog');
-const todoItemDetailsDialog = document.querySelector('.todoItemDetailsDialog');
 
-const todoDetailsBtns = document.querySelectorAll('.showTodoDetailsBtn');
 
+// INIT
 newTodoItem.addEventListener('click', () => {
     createTodoItemDialog.style.display = "flex";
 });
@@ -13,11 +54,5 @@ newTodoItem.addEventListener('click', () => {
 forms.forEach(form => {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-    });
-});
-
-todoDetailsBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        todoItemDetailsDialog.style.display = "flex";
     });
 });
