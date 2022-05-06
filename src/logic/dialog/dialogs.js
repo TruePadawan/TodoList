f13c70db9f81167dc89cd4d09f8ab195aec6bf89
TruePadawan <hermeschigoziri@gmail.com>
2022-05-06 23:31:32 +0100

    update dialog functionality
    update dialog functionality to account for more than one dialog

---
+++ src/logic/dialog/dialogs.js
@@ -0,0 +1,17 @@
+const dialogs = document.querySelectorAll('dialog');
+const forms = document.querySelectorAll('dialog > form');
+
+
+// INIT
+dialogs.forEach(dialog => {
+    dialog.addEventListener('click', () => {
+        dialog.style.display = "none";
+    });
+});
+    
+
+forms.forEach(form => {
+    form.addEventListener('click', (e) => {
+        e.stopPropagation();
+    });
+});
