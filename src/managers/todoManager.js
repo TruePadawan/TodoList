612fe0da516c4bd14b04af3a45c3440d35efa9b3
Hermes <hermeschigoziri@gmail.com>
2022-05-07 23:50:01 +0100

    add functionality for adding todo items to projects

--- src/managers/todoManager.js
+++ src/managers/todoManager.js
@@ -3,10 +3,7 @@ import { eventManager } from "./eventManager";
 import { projectsList } from "../global_data";
 
 class TodoManager {
-    #todoContainer;
-
-    constructor() {
-        this.#todoContainer = document.querySelector('.todos');
+    constructor() {  
     }
 
     addTodoItem = (todoItem, projectID) => {
@@ -14,10 +11,8 @@ class TodoManager {
         {
             let id = uuidv4();
             projectsList[projectID].todos[id] = todoItem;
-            let title = projectsList[projectID].title;
-            let todos = projectsList[projectID].todos;
-
-            console.log(`Project [${title}]`, todos);
+            // let title = projectsList[projectID].title;
+            // let todos = projectsList[projectID].todos;
             eventManager.triggerEvent('todoItemAdded', [projectID]);
         }
     };
