ad2fdca627db49d61a12f1e0e385cbc0c5727eb2
Hermes <hermeschigoziri@gmail.com>
2022-05-08 11:18:06 +0100

    Update global_data.js

--- src/global_data.js
+++ src/global_data.js
@@ -1,1 +1,19 @@
 export const projectsList = {};
+
+export function resetElements(elements) {
+    elements.forEach(element => {
+        element.value = "";
+    });
+}
+
+export function createTodoItem(props)
+{
+    let todoItem = {
+        title: props.title,
+        dueDate: props.dueDate,
+        priority: props.priority,
+        desc: props.desc
+    }
+
+    return todoItem;
+}
