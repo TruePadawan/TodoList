b8d615e40483a7f838f6bbea7cb0b938209af721
Hermes <hermeschigoziri@gmail.com>
2022-05-10 19:34:29 +0100

    minor changes

--- src/managers/dataManager.js
+++ src/managers/dataManager.js
@@ -1,13 +1,3 @@
-/* Simple functions for easily adding objects and array to localStorage */
-Storage.prototype.setObj = function (key, obj) {
-    return this.setItem(key, JSON.stringify(obj));
-};
-
-Storage.prototype.getObj = function (key) {
-    return JSON.parse(this.getItem(key));
-};
-
-
 export function storeProjectDataLocally(projects) {
     localStorage.setObj('projects', projects);
 }
