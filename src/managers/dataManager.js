fa40fda99e5fb99b93fd00d9879f3721e2aedf89
Hermes <hermeschigoziri@gmail.com>
2022-05-10 21:57:30 +0100

    remove irrelevant function

--- src/managers/dataManager.js
+++ src/managers/dataManager.js
@@ -1,7 +1,3 @@
 export function storeProjectDataLocally(projects) {
     localStorage.setObj('projects', projects);
 }
-
-export function retriveProjectDataLocally() {
-    return getObj('projects');
-}
